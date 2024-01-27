a7ad7c1450070c1d9794808adec956e1cb16772e
shabeebtk <shabeebtk768@gmail.com>
2024-01-27 14:00:26 +0530

    changed base url

--- src/constant/BaseUrl.js
+++ src/constant/BaseUrl.js
@@ -1,10 +1,10 @@
-export const BaseUrl = 'http://127.0.0.1:8000/'
-export const ImageUrl = 'http://127.0.0.1:8000'
-export const webSocketUrl = 'ws://127.0.0.1:8000/ws/chat/'
+// export const BaseUrl = 'http://127.0.0.1:8000/'
+// export const ImageUrl = 'http://127.0.0.1:8000'
+// export const webSocketUrl = 'ws://127.0.0.1:8000/ws/chat/'
 
-// export const BaseUrl = 'http://52.66.242.228/'
-// export const ImageUrl = 'http://52.66.242.228'
-// export const webSocketUrl = 'ws://52.66.242.228/ws/chat/'
+export const BaseUrl = 'http://52.66.242.228/'
+export const ImageUrl = 'http://52.66.242.228'
+export const webSocketUrl = 'ws://52.66.242.228/ws/chat/'
 
 
 
