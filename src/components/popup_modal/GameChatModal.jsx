931e5d341fcdafbee4d85aaddbdba88a32d99ea7
shabeebtk <shabeebtk768@gmail.com>
2024-01-31 17:38:39 +0530

    changed ws url3

--- src/components/popup_modal/GameChatModal.jsx
+++ src/components/popup_modal/GameChatModal.jsx
@@ -46,7 +46,8 @@ function GameChatModal(props) {
     useEffect(() => {
         getMessages()
         const ws_scheme = window.location.protocol == 'https' ? 'wss://' : 'wss://'
-        const ws = new WebSocket(`${ws_scheme}${webSocketUrl}${props.game.id}/`);
+        // const ws = new WebSocket(`${ws_scheme}${webSocketUrl}${props.game.id}/`);
+        const ws = new WebSocket(`ws://http://www.playoff-turf.online/ws/chat/${props.game.id}`);
         
         alert(`${ws_scheme}${webSocketUrl}${props.game.id}/`)
         ws.onopen = () => {
